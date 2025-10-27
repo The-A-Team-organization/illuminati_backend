49334d534f196521a3797dcdf5bbe0bc49755b98
obulka <oleg.bulka@gmail.com>
2025-10-27 10:58:43 +0200

    TAT-24: add new endpoint /records/create for creating records

--- apps/records/urls.py
+++ apps/records/urls.py
@@ -3,4 +3,5 @@ from .views import RecordListView
 
 urlpatterns = [
     path("all", RecordListView.as_view(), name="records-all"),
+    path("create", RecordCreateView.as_view(), name="records-create"),
 ]
