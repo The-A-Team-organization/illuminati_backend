2f85785f2a93e5535739ee5462bae1e1a70c321d
obulka <oleg.bulka@gmail.com>
2025-10-28 10:07:37 +0200

    TAT-34: add new view to urls.py

--- apps/records/urls.py
+++ apps/records/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RecordListView, RecordCreateView
+from .views import RecordListView, RecordCreateView, RecordDetailView
 
 urlpatterns = [
     path("all", RecordListView.as_view(), name="records-all"),
     path("create", RecordCreateView.as_view(), name="records-create"),
+    path("<int:record_id>", RecordDetailView.as_view(), name="records-detail"),
 ]
