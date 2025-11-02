b8eeefd95e7311e721ba9fd84b1806e4e50a8334
obulka <oleg.bulka@gmail.com>
2025-11-02 11:29:34 +0200

    TAT-54: connect main routing file to new module

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("users/", include("apps.users.urls")),
     path("entry_password/", include("apps.entry_password.urls")),
     path("records/", include("apps.records.urls")),
-    path('snapshot/', include('apps.snapshot.urls')),
-    path('votes/', include('apps.votes.urls'))
+    path("snapshot/", include("apps.snapshot.urls")),
+    path("votes/", include("apps.votes.urls")),
+    path("hall_of_fame/", include("apps.hall_of_fame.urls")),
 ]
