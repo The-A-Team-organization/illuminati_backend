85cd1be79b1175f6d4fcb28a6c80c6a4c3be7a26
obulka <oleg.bulka@gmail.com>
2025-11-02 19:51:45 +0200

    TAT-54: add logic for sending email via go service in services.py

--- apps/hall_of_fame/services.py
+++ apps/hall_of_fame/services.py
@@ -1,4 +1,5 @@
 from .models import HallOfFame
+import requests
 
 
 def get_all_architects():
@@ -8,7 +9,25 @@ def get_all_architects():
 def send_message_to_architect(architect_id, message):
     try:
         architect = HallOfFame.objects.get(id=architect_id)
-        print(f"[MOCK] Sent message to {architect.email}: {message}")
+        payload = {
+            "topic": "Message from current Architect",
+            "text": message,
+            "target_emails": [architect.email],
+        }
+        try:
+            response = requests.post(
+                "http://docker_go:8080/send_letter",
+                json=payload,
+                timeout=3,
+            )
+            if response.status_code != 200:
+                print(f"Mailer error: {response.text}")
+            else:
+                print(f"Message successfully sent to {architect.email}")
+        except Exception as e:
+            print(f"Failed to send mail via Go service: {e}")
+
         return True
+
     except HallOfFame.DoesNotExist:
         return False
